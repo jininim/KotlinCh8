2c3342265d1dddf3c8cf479796d4e6c840785c7b
developer Jin <91578450+jininim@users.noreply.github.com>
2022-08-10 23:50:36 +0900

    탐색 및 백스택 추가

--- cupcake/src/main/java/com/example/cupcake/MainActivity.kt
+++ cupcake/src/main/java/com/example/cupcake/MainActivity.kt
@@ -17,6 +17,7 @@ package com.example.cupcake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -25,6 +26,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main){
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,5 +35,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main){
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- cupcake/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ cupcake/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -62,7 +62,7 @@ class OrderViewModel : ViewModel(){
     fun hasNoFlavorSet(): Boolean{
         return _flavor.value.isNullOrEmpty()
     }
-    private fun resetOrder(){
+     fun resetOrder(){
         _quantity.value = 0
         _flavor.value = ""
         _date.value = dateOptions[0]
