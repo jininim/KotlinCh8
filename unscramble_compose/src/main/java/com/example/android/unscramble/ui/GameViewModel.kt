16d85ff93be2f068ec871d60e3ddae073e1704aa
developer Jin <91578450+jininim@users.noreply.github.com>
2022-12-28 23:49:44 +0900

    Cupcake_compose Compose를 사용하여 화면 간 이동

---
+++ unscramble_compose/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.android.unscramble.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+    var userGuess by mutableStateOf("")
+        private set
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while(String(tempWord) == word){
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+    private fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+    fun updateUserGuess(guessedWord: String){
+        userGuess = guessedWord
+    }
+    init {
+        resetGame()
+    }
+}
